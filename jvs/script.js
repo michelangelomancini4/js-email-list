40b7293a8a87ebfc084d5b5447cde2777a2d0106
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-22 15:46:00 +0100

    cambio metodo di chiamata da fetch ad axios

--- jvs/script.js
+++ jvs/script.js
@@ -1,12 +1,33 @@
+//   richiamo la variabile per output
+
+const outputEl = document.getElementById('outputElement');
+
+
+// creo ciclo per generare 10 mail diverse
+for (let i = 0; i < 10; i++) {
+
 // generazione email random tramite API
 
-fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-  .then(response => response.json()) 
-  .then(data => {
+axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+.then((response) => {
+let result = response.data;
+  // stampo in console le email
+  console.log("Indirizzo email:", result.response); 
 
-    // stampo in console l'email
-    console.log("Indirizzo email:", data.response); 
-  })
-  .catch(error => {
-    console.error(error);
-  });
+   // stampo in pagina le mail
+  outputEl.innerHTML += `${"Indirizzo email:"} ${ result.response}`;
+})
+ 
+// in caso di errore stampo in console l'errore
+
+.catch(error => {
+  console.error(error);
+});
+}
+
+
+
+
+
+
+
